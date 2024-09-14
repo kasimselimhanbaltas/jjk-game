d3596e532716512ffaf0db0e11ea4fcf99341960
kasimselimhanbaltas <selimhanbaltas@gmail.com>
2024-09-15 00:29:47 +0300

    electron, gojo after purple merge pose fix, domain loop bug fix, starting controls page

---
+++ src/Pages/ControlsPage.tsx
@@ -0,0 +1,32 @@
+
+
+const ControlsPage = () => {
+
+    return (
+        <div>
+            <div className="controls-page">
+                <div className="controls-text">
+                    <>
+                        <p>Controls: </p>
+                        <p> W-A-S-D: Movement</p>
+                        <p> Space: Dash</p>
+                        <p> Q - Block / Domain amplification</p>
+                        <p> E - Blue </p>
+                        <p> R - Red </p>
+                        <p style={{ color: "gold" }}> E + R - Purple </p>
+                        <p> J - Punch Combo</p>
+                        <p> S + J - Kick Combo</p>
+                        <p> K - Black Flash Combo</p>
+                        <p style={{ color: "gold" }}> SHIFT + E/R - Charge Skills </p>
+                        <p> G - Toggle Falling Blossom Emotion </p>
+                        <p> C - Simple Domain </p>
+                        <p> V - Toggle Infinity </p>
+                        <p> Z - Heal(Reversed Cursed Energy)</p>
+                        <p> X - Heal Burnt Out Cursed Technique(Reversed Cursed Energy)</p>
+                    </>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default ControlsPage;
